c1d5cea4b9e879f3acc480af3a5f395794d11ef4
Yves Talboom <yves.talboom@gmail.com>
2016-02-21 09:07:01 +0100

    Initial commit

---
+++ src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -0,0 +1,20 @@
+package masterSpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class MasterSpringMvcApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSpringMvcApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MasterSpringMvcApplication.class);
+	}
+}
